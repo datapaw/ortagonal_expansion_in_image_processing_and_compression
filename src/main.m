5fc850bdfe8353c5d2c8fa597623275691d342a6
DabrowickiWojciech <dabrowicki.wojciech@gmail.com>
2024-03-14 12:57:03 +0100

    quantizations

--- src/main.m
+++ src/main.m
@@ -11,30 +11,30 @@ figure;imshow(Image)
 ...imwrite(Image,"images/grayed.jpg")
 
 %% Downsampling and other simple solutions
-%downsampling by how many times
+% Downsampling by how many times
 DS = 3;
 
-%obtaining the size of the image using:
-%size(rows/column/page)
-%Row - number of rows in the image / height
-%Column - number of columns / width
-%Page - encodes the three color channels RGB
+% Obtaining the size of the image using:
+% size(rows/column/page)
+% Row - number of rows in the image / height
+% Column - number of columns / width
+% Page - encodes the three color channels RGB
 [Height, Width, RGB] = size(Image);
 
-%sampling calculations
-%DS - downsampling
+% Sampling calculations
+% DS - downsampling
 DSimage_1 = Image(1:DS:Height, 1:DS:Width);
 figure; imshow(DSimage_1); title(["Downsampled by" num2str(DS)])
 ...imwrite(DSimage_1,"images/DSimage_1.jpg")
 
-%downsampling and resizing up
-%RU - resizing up
+% Downsampling and resizing up
+% RU - resizing up
 DSRUimage_1 = imresize(DSimage_1,[Height,Width]);
 figure;imshow(DSRUimage_1); title("Downsampling and resizing up")
 ...imwrite(DSRUimage_1,"images/DSRUimage_1.jpg")
 
-%downsampling and resizing up with filter
-%RUF - resizing up + filter
+% Downsampling and resizing up with filter
+% RUF - resizing up + filter
 RUFimage_1 = imfilter(DSRUimage_1, fspecial("laplacian"));
 figure
 subplot(2,1,1);imshow(RUFimage_1); title("Downsampling and resizing up with laplacian filter")
@@ -43,9 +43,41 @@ RUFimage_2 = imfilter(DSRUimage_1, fspecial("gaussian",6,2));
 subplot(2,1,2);imshow(RUFimage_2); title("Downsampling and resizing up with gaussian filter")
 ...imwrite(RUFimage_2,"images/RUFimage_2.jpg")
 
-%% Lloyd-Max Quantizer (uniform quantizer)
-% desired number of bits for quantization (1-8)
-% 1-2|2-4|3-8|4-16|5-32|6-64|7-128|8-256
+%% Uniform Quantizer
+% Desired number of bits for quantization (4-64)
+% 4|8|16|32|64
+UQ_bits = 16;
+
+% Convert the image to double precision for calculations
+UQ_values = double(Image);
+
+% Flatten the image into a 1D array
+UQ_input_values = UQ_values(:);
+
+% Compute quantization step size
+UQ_step_size = (max(UQ_input_values) - min(UQ_input_values)) / (UQ_bits - 1);
+
+% Compute quantization intervals
+UQ_quantization_intervals = min(UQ_input_values):UQ_step_size:max(UQ_input_values);
+
+% Quantize input values based on quantization intervals
+UQ_quantized_values = zeros(size(UQ_input_values));
+for i = 1:UQ_bits
+    % Assign input values to quantization levels based on intervals
+    UQ_quantized_values(UQ_input_values >= UQ_quantization_intervals(i)) = i - 1;
+end
+
+% Reshape quantized values back to image dimensions
+UQimage = reshape(UQ_quantized_values, size(Image));
+
+% Display original and quantized images
+figure;subplot(1, 2, 1);imshow(uint8(Image));title('Original Image');
+subplot(1, 2, 2);imshow(uint8(UQimage * (255 / (UQ_bits - 1))));title(['Quantized Image (Uniform, ', num2str(UQ_bits), ' levels)']);
+...imwrite(uint8(UQimage),"images/UQimage.jpg")
+
+%% Lloyd-Max Quantizer
+% Desired number of bits for quantization (1-8)
+% 2|4|8|16|32|64|128|256
 LMQ_bits = 4;
 
 % Convert the image to double precision for calculations
@@ -57,7 +89,7 @@ LMQ_input_values = LMQ_values(:);
 % Initialize centroids based on the desired number of quantization levels
 LMQ_min_value = min(LMQ_input_values);
 LMQ_max_value = max(LMQ_input_values);
-LMQ_centroids = linspace(LMQ_min_value, LMQ_max_value, 2^LMQ_bits);
+LMQ_centroids = linspace(LMQ_min_value, LMQ_max_value, LMQ_bits);
 
 % Iterative Lloyd-Max algorithm
 max_iterations = 128;
@@ -97,9 +129,9 @@ subplot(1, 2, 2); imshow(uint8(LMQimage)); title(['Quantized Image (' num2str(LM
 ...imwrite(uint8(LMQimage),"images/LMQimage.jpg")
 
 %% NonUniform Quantizer
-% desired number of bits for quantization (2-384)
+% Desired number of bits for quantization (2-384)
 % 32|64|128|256|384
-NU_bits = 256;
+NU_bits = 32;
 
 % Convert the image to double precision for calculations
 NU_values = double(Image);
@@ -113,6 +145,7 @@ NU_max_value = max(NU_input_values);
 
 % Compute histogram of input image
 [counts, bins] = imhist(uint8(NU_values));
+...figure;plot(bins, counts);title('Histogram of inputs');
 
 % Compute cumulative histogram
 NU_cumulative_counts = cumsum(counts);
@@ -136,6 +169,13 @@ NU_quantization_intervals = interp1(NU_cdf, bins, linspace(NU_min_value/NU_max_v
 % Quantize input values based on quantization intervals
 NU_quantized_values = zeros(size(NU_input_values));
 for i = 1:(NU_bits - 1)
+    % This loop quantizes the input pixel values based on the quantization
+    % intervals computed earlier. It assigns each input value to the 
+    % corresponding quantization level based on the intervals. The loop
+    % iterates over each interval except the last one (handled separately)
+    % and assigns the quantization level i to the input values falling 
+    % within the interval 
+    
     % Assign input values to quantization levels based on intervals
     NU_quantized_values(NU_input_values >= NU_quantization_intervals(i) & NU_input_values < NU_quantization_intervals(i+1)) = i;
 end
@@ -150,7 +190,7 @@ figure;subplot(1, 2, 1);imshow(uint8(Image));title('Original Image');
 subplot(1, 2, 2);imshow(uint8(NUimage));title(['Quantized Image (Non-uniform, ', num2str(NU_bits), ' levels)']);
 ...imwrite(uint8(NUimage),"images/NUimage.jpg")
 
-%% Dithering
+%% Image transforms
 
 
 
